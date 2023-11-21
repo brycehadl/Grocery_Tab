3717efc9a8cc490a8ed9070dbde403ee2fc88541
Zach Barnes <16zacharybarnes@gmail.com>
2023-11-20 21:09:43 -0600

    fixed actions inside utils, updated project-boilerplate to grocerytab

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -3,9 +3,15 @@ export const USER_PROFILE = "USER_PROFILE";
 
 export const SIGNIN = "SIGNIN";
 export const LOGIN = "LOGIN";
+
 export const ADD_USER = "ADD_USER";
-export const UPDATE_USER = "UPDATE_USER";
 export const ADD_ORDER = "ADD_ORDER";
 export const ADD_PRODUCT = "ADD_PRODUCT";
+
+export const UPDATE_USER = "UPDATE_USER";
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+
+export const REMOVE_ORDER = "REMOVE_ORDER";
+export const REMOVE_PRODUCT = "REMOVE_PRODUCT";
+
 export const CREATE_STORE = "CREATE_STORE";
