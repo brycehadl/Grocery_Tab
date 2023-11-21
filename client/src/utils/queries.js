5b7c92102a2df9022d977cfaf69a3fb730c3ed23
Zach Barnes <16zacharybarnes@gmail.com>
2023-11-20 19:50:25 -0600

    added queries and updated existing query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,12 +1,91 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
-  {
+  query user {
     user {
       _id
       firstName
       lastName
       email
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+  query users {
+    users {
+      _id
+      firstName
+      lastName
+      email
+    }
+  }
+`;
+
+export const QUERY_PRODUCT = gql`
+  query product($_id: ID!) {
+    product(_id: $_id) {
+      _id
+      name
+      price
+      description
+      image
+      quantity
+      category
+    }
+  }
+`;
+
+export const QUERY_PRODUCTS = gql`
+  query products {
+    products {
+      _id
+      name
+      price
+      description
+      image
+      quantity
+      category
+    }
+  }
+`;
+
+export const QUERY_STORE = gql`
+  query store($_id: ID!) {
+    store(_id: $_id) {
+      _id
+      storeName
+      storeOwner
+      products
+    }
+  }
+`;
+
+export const QUERY_STORES = gql`
+  query stores {
+    stores {
+      _id
+      storeName
+      storeOwner
+      products
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query checkout($products: ID!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ORDER = gql`
+  query order($_id: ID!) {
+    order(_id: $_id) {
+      _id
+      purchaseDate
+      products
     }
   }
 `;
