769058592e5255eb8391e447021e370ae194a84e
Zach Barnes <16zacharybarnes@gmail.com>
2023-11-29 18:27:23 -0600

    added title to html, made recent purchases look nicer

--- client/src/pages/Recent_Purchases/index.jsx
+++ client/src/pages/Recent_Purchases/index.jsx
@@ -1,13 +1,12 @@
 import './style.scss';
 
 export default function RecentPurchases() {
+
+
+
   return (
     <div id="recent-purchases-page">
       <h1>Recent Purchases</h1>
-
-      <p>
-        example text
-      </p>
     </div>
   );
 };
