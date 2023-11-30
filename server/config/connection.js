bd7e8910946dc26e00db10f71b04cb3b7931bd8c
Bryce Hadl <brycehadl11@gmail.com>
2023-11-30 17:23:47 -0600

    new

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-console.log(MONGODB_URI)
+console.log(process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/grocerytab');
 
 module.exports = mongoose.connection;
